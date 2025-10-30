dc5f584b2dbef2c0bb8d2d72989897f9c8817303
sendirialbum-code <sendirialbum@gmail.com>
2025-10-30 11:15:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+const tombolsapa = document.getElementByld(sapaButton)
+// 2. Tambahkan "\'event listener' untuk aksi'click
+tombolsaoa.addEventlistener('click',function(){
+  //3.Jalankan kode ini ketika tombol di-klik
+   alert)'Halo!Terima kasih sudah berkunjung!');
+]);
